a4bf94f97c0871ce5a2ee58c1cb639f1876c8e72
DEEPAK KUMAR <78249949+kumardeepak85690@users.noreply.github.com>
2023-12-19 08:41:48 +0000

    commit

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,63 @@
+let score = "33abc"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueInNumber = Number(score)
+
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; flase => 0
+// Null = > 0
+// undefined => NaN
+// string => NaN
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+// console.log(booleanIsLoggedIn);
+
+// 1 => true ; 0 => false
+// "" => false
+// "Deepak" => true
+
+let someNum = 33
+let stringNum = String(someNum)
+// console.log(stringNum);
+// console.log(typeof stringNum);
+
+
+//************************ Operations ******************************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2%2);
+// console.log(2**2);
+
+let str1 = "Hello"
+let str2 =" Deepak"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log(+true);
+
+let gameCounter = 1
+// console.log(gameCounter++); //postfix
+// console.log(gameCounter);
+// console.log(++gameCounter); //prefix
