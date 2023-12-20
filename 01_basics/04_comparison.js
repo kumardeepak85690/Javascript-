d7fbce9365dfbe9f10c9ff6523503cae89564ed6
DEEPAK KUMAR <78249949+kumardeepak85690@users.noreply.github.com>
2023-12-20 06:07:01 +0000

    done

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,20 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2<=1);
+// console.log(2==1);
+// console.log(2!=1);
+
+
+// console.log("2" > 1);  //avoid 
+// console.log("02" > 1);  //avoid 
+
+// console.log(null > 0);   //avoid 
+// console.log(null == 0);  //avoid 
+// console.log(null >= 0);  //avoid 
+
+// console.log(undefined == 0);  //avoid 
+
+// === 
+
+console.log("2" === 2); //campare both value and data type
