2bf7e899658326e8dd1c22ba8ba56f774a246a77
DEEPAK KUMAR <78249949+kumardeepak85690@users.noreply.github.com>
2023-12-20 06:32:54 +0000

    datatype summry

---
+++ 01_basics/datatype_summery.js
@@ -0,0 +1,38 @@
+// Primitive : 7 types
+
+    // String  = string
+        let name = "Deepak"
+
+    // Symbol = symbol
+        let id = Symbol('4961')
+
+    // BigInt = bigint
+        let bigNum = 49610000000n
+
+    // Boolean = boolean
+        let loggedIn = true
+
+    // Number = number
+        let num = 123
+
+    // null  = object
+        let value = null
+
+    // undefined = undefind
+        let roomTemp = undefined
+
+// Reference  (Non Primitive)
+
+    // Array
+        const heros = ["thor", "ironMan", "spiderMan"];
+
+    // Object
+        let myObj = {
+            name: "Deepak",
+            age: 23
+        }
+
+    // Function
+        const myFun = function(){
+            console.log("hello world");
+        }
