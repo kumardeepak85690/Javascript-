6a2010238a96a9d23dbe693a4dcbafb1ba81883e
DEEPAK KUMAR <78249949+kumardeepak85690@users.noreply.github.com>
2023-12-21 12:52:41 +0000

    date

---
+++ 01_basics/07_dates.js
@@ -0,0 +1,39 @@
+let myDates = new Date()
+// console.log(myDates);
+
+console.log(myDates.toString());
+// console.log(myDates.toDateString);
+// console.log(myDates.toLocaleString);
+// console.log(myDates.toLocaleDateString);
+// console.log(myDates.toISOString);
+// console.log(myDates.toJSON);
+// console.log(typeof myDates);
+
+// let myCreatedDate = new Date(2000, 0, 26)
+// let myCreatedDate = new Date(2000, 6, 26, 5, 3)
+// let myCreatedDate = new Date("2023-01-14")
+let myCreatedDate = new Date("01-01-2024")
+
+// console.log(myCreatedDate.toDateString());
+// console.log(myCreatedDate.toLocaleString());
+
+let myTimeStamp = Date.now()
+
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+
+// console.log(Date.now());
+// console.log(Math.floor(Date.now()/1000));
+
+let newDate = new Date()
+
+// console.log(newDate);
+// console.log(newDate.getDay());
+// console.log(newDate.getMonth() + 1);
+// console.log(newDate.getFullYear());
+
+// '${newDate.getDay()} and the time' 
+
+// newDate.toLocaleString('default',{
+//     weekday: "long"
+// })
